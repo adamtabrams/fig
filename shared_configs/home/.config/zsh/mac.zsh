204c9917d702ecd12f2a898e3a25dfc2170b3a53
Adam Abrams <adam.abrams@optum.com>
2021-09-22 14:14:55 -0500

    update lf, zsh, and vimium configs

--- shared_configs/home/.config/zsh/mac.zsh
+++ shared_configs/home/.config/zsh/mac.zsh
@@ -1,7 +1,9 @@
 export BROWSER="/Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser"
 
-which gdircolors &>/dev/null && eval $(gdircolors $ZDOTDIR/dircolors.ansi-dark)
-
-FontSmoothing=$(defaults read -g CGFontRenderingFontSmoothingDisabled) 2&>/dev/null
+FontSmoothing=$(defaults read -g CGFontRenderingFontSmoothingDisabled 2&>/dev/null)
 [ "$FontSmoothing" = 1 ] &&
     defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+
+which gdircolors &>/dev/null &&
+    eval $(gdircolors $ZDOTDIR/dircolors.ansi-dark) ||
+    echo "need 'gdircolors' program to set terminal colors" >&2
