0a3f34ce3082d0dbabdcabd82a6216e5f962d49b
Adam Abrams <adam.abrams@optum.com>
2022-02-03 17:58:04 -0600

    feat: improve zsh docker functions

--- shared_configs/home/.config/zsh/docker.zsh
+++ shared_configs/home/.config/zsh/docker.zsh
@@ -1,11 +1,20 @@
 # list running container names
-dps() { docker ps | awk 'NR>1 {print $NF}' }
+dockerps() { docker ps | awk 'NR>1 {print $NF}' }
 
 # list all container names
-dpsa() { docker ps -a | awk 'NR>1 {print $NF}' }
+dockerpsa() { docker ps -a | awk 'NR>1 {print $NF}' }
+
+# list all container names catergorized
+dps() {
+    containers=$(dockerps; dockerpsa)
+    running=$(echo "$containers" | sort | uniq -d)
+    stopped=$(echo "$containers" | sort | uniq -u)
+    [ "$running" ] && printf "----------\n RUNNING:\n----------\n%s\n" "$running"
+    [ "$stopped" ] && printf "----------\n STOPPED:\n----------\n%s\n" "$stopped"
+}
 
 # remove running containers
-drm() { docker rm -vf $(dps | tr "\n" " ") }
+drm() { docker rm -vf $(dockerps | tr "\n" " ") }
 
 # remove all containers
-drma() { docker rm -vf $(dpsa | tr "\n" " ") }
+drma() { docker rm -vf $(dockerpsa | tr "\n" " ") }
